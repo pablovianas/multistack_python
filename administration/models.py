4b2fbe07c459837de224cb0e403e92c6c8bcd4f3
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-02-04 22:03:27 -0300

    Service Model creation

---
+++ administration/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from sqlalchemy import false, null
+
+# Create your models here.
+
+class Service(models.Model):
+    ICON_CHOICES = (
+        ('twf_cleaning-1', 'twf_cleaning-1'),
+        ('twf_cleaning-2', 'twf_cleaning-2'),
+        ('twf_cleaning-3', 'twf_cleaning-3'),
+    )
+
+
+    name = models.CharField(max_length=50, null=False, blank=False)
+    min_value = models.FloatField(null=False, blank=False)
+    qtt_hours = models.IntegerField(null=False, blank=False)
+    porcentage_comission = models.FloatField(null=False, blank=False)
+    hours_bedroom = models.IntegerField(null=False, blank=False)
+    value_bedroom = models.FloatField(null=False, blank=False)
+    hours_room = models.IntegerField(null=False, blank=False)
+    value_room = models.FloatField(null=False, blank=False)
+    hours_bathroom = models.IntegerField(null=False, blank=False)
+    value_bathroom = models.FloatField(null=False, blank=False)
+    hours_kitchen = models.IntegerField(null=False, blank=False)
+    value_kitchen = models.FloatField(null=False, blank=False)
+    hours_yard = models.IntegerField(null=False, blank=False)
+    value_yard = models.FloatField(null=False, blank=False)
+    hours_others = models.IntegerField(null=False, blank=False)
+    value_others = models.FloatField(null=False, blank=False)
+    icon = models.CharField(null=False, blank=False, choices=ICON_CHOICES, max_length=14)
+    position = models.IntegerField(null=False)
