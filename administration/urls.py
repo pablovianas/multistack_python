727e11db426dcb618318a1df9b4b114dfebd44f6
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-02-05 21:15:11 -0300

    Added services list urls

--- administration/urls.py
+++ administration/urls.py
@@ -1,8 +1,10 @@
 import imp
 from unicodedata import name
 from django.urls import path
-from .views import service_register
+from .views import service_register, service_list, edit_service
 
 urlpatterns = [
-    path('services/register', service_register, name="service_register")
+    path('services/register', service_register, name="service_register"),
+    path('services/list', service_list, name='service_list'),
+    path('services/edit/<int:id>', edit_service, name='edit_service'),
 ]
