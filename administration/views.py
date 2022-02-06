72c20fb08536e58b98ec17276d1307a9ffcf2524
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-02-05 21:15:52 -0300

    Created list and edit views for services

--- administration/views.py
+++ administration/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import ServiceForm
+from .models import Service
 
 # Create your views here.
 
 def service_register(request):
-    form_service = ServiceForm()
+    if request.method == 'POST':
+        form_service = ServiceForm(request.POST)
+        if form_service.is_valid():
+            form_service.save()
+            return redirect('service_list')
+    else:
+        form_service = ServiceForm()
+
+    return render(request, 'services/form_service.html', {'form_service': form_service})
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'services/service_list.html', {'services': services})
+
+def edit_service(request, id):
+    service = Service.objects.get(id=id)
+    form_service = ServiceForm(request.POST or None, instance=service)
+    if form_service.is_valid():
+        form_service.save()
+        return redirect('service_list')
     return render(request, 'services/form_service.html', {'form_service': form_service})
