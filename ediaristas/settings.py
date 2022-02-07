4d796e2f9f3f0b8e33ae939ad2f0615063343774
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-02-06 21:33:56 -0300

    Updated Lang code and installed apps"

--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'adminlte3',
+    'bootstrap4',
     
 ]
 
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
