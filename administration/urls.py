87dc8eed8745bff81cc0f64e5d5ba9e99db26e02
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-02-09 12:48:12 -0300

    List users done

--- administration/urls.py
+++ administration/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('services/register', service_views.service_register, name="service_register"),
     path('services/list', service_views.service_list, name='service_list'),
     path('services/edit/<int:id>', service_views.edit_service, name='edit_service'),
-    path('user/register', user_views.user_register, name='user_register')
+    path('user/register', user_views.user_register, name='user_register'),
+    path('user/list', user_views.user_list, name='user_list'),
 ]
--- administration/views/user_views.py
+++ administration/views/user_views.py
@@ -1,13 +1,19 @@
 from ..forms.user_forms import UserForms
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
 
 def user_register(request):
     if request.method == 'POST':
         user_form = UserForms(request.POST)
         if user_form.is_valid():
             user_form.save()
+            return redirect('user_list')
     else:
         user_form = UserForms()
 
-    return render(request, 'usuarios/user_form.html', {'user_form': user_form})
+    return render(request, 'users/user_form.html', {'user_form': user_form})
+
+def user_list(request):
+    User = get_user_model()
+    users = User.objects.filter(is_superuser=True)
+    return render(request, 'users/user_list.html', {'users': users})
